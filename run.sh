92d26bfea0721d82389b176dd891585eff1cc36e
Mario Negro Ponzi <mario.negro.ponzi@nl.abnamro.com>
2024-05-10 14:35:20 +0200

    simplified for multiple languages

--- run.sh
+++ run.sh
@@ -1,59 +1,60 @@
 #!/bin/zsh
 
-export COUNT=100
+export COUNT=100000
 
-mkdir -p {dart,go,java}
+mkdir -p {dart,go,java,c,swift}
 
 echo '\nDart (interpreted)'
-./hyperfine 'dart hello.dart' --warmup 10 --runs 100
+# ./hyperfine 'dart hello.dart' --warmup 2 --runs 10
 gtime -f "%es %MkB" dart hello.dart
 
 echo '\nDart (AOT compiled)'
 dart compile exe hello.dart -o dart/hello
-./hyperfine 'dart/hello' --warmup 10 --runs 100
+# ./hyperfine 'dart/hello' --warmup 2 --runs 10
 gtime -f "%es %MkB" dart/hello
 
 echo '\nGo (interpreted)'
-./hyperfine 'go run hello.go' --warmup 10 --runs 100
+# ./hyperfine 'go run hello.go' --warmup 2 --runs 10
 gtime -f "%es %MkB" go run hello.go
 
 echo '\nGo (compiled)'
 go build -o go hello.go
-./hyperfine 'go/hello' --warmup 10 --runs 100
+# ./hyperfine 'go/hello' --warmup 2 --runs 10
 gtime -f "%es %MkB" go/hello
 
 echo '\nJava (compiled)'
 javac -d java hello.java
-./hyperfine 'java -cp java HelloWorld' --warmup 10 --runs 100
+# ./hyperfine 'java -cp java HelloWorld' --warmup 2 --runs 10
 gtime -f "%es %MkB" java -cp java HelloWorld
 
 echo '\nNode'
-./hyperfine 'node hello.js' --warmup 10 --runs 100
+# ./hyperfine 'node hello.js' --warmup 2 --runs 10
 gtime -f "%es %MkB" node hello.js
 
 echo '\nPython 3'
-./hyperfine 'python3 hello.py' --warmup 10 --runs 100
+# ./hyperfine 'python3 hello.py' --warmup 2 --runs 10
 gtime -f "%es %MkB" python3 hello.py
 
 echo '\nRuby'
-./hyperfine 'ruby hello.rb' --warmup 10 --runs 100
+# ./hyperfine 'ruby hello.rb' --warmup 2 --runs 10
 gtime -f "%es %MkB" ruby hello.rb
 
-echo 'Shell [skipped - too slow]'
-# ./hyperfine './hello.sh' --warmup 10 --runs 100
+echo '\nShell [skip]'
+# ./hyperfine './hello.sh' --warmup 2 --runs 10
 
-echo 'Swift (interpreted) [skipped - too slow]'
-# ./hyperfine 'swift hello.swift' --warmup 10 --runs 100
+echo '\nSwift (interpreted)'
+# ./hyperfine 'swift hello.swift' --warmup 2 --runs 10
+gtime -f "%es %MkB" swift -O hello.swift
 
 echo '\nSwift (compiled)'
-swiftc -o swift/hello hello.swift
-./hyperfine 'swift/hello' --warmup 10 --runs 100
+swiftc -O -o swift/hello hello.swift
+# ./hyperfine 'swift/hello' --warmup 2 --runs 10
 gtime -f "%es %MkB" swift/hello
 
 echo '\nC'
-gcc -o C/hello hello.c
-./hyperfine 'C/hello' --warmup 10 --runs 100
-gtime -f "%es %MkB" C/hello
+gcc -O -o c/hello hello.c
+# ./hyperfine 'C/hello' --warmup 2 --runs 10
+gtime -f "%es %MkB" c/hello
 
 
 
