374f5536d6a9d6bba8d5386bbdf629d49c504555
Mario Negro Ponzi <mario.negro.ponzi@nl.abnamro.com>
2024-05-10 12:44:33 +0200

    Updated dart to primecount

--- hello.dart
+++ hello.dart
@@ -1,3 +1,23 @@
 void main() {
-  print('Hello World!');
+  const count = 10000;
+  final primes = fillPrimesTil(count);
+  print("${primes.length} primes between 1 and $count");
+}
+
+List<int> fillPrimesTil(int i) {
+  List<int> l = [1];
+  for (int count = 0; count < i; count++) {
+    if (isPrime(count)) {
+      l.add(count);
+    }
+  }
+  return l;
+}
+
+bool isPrime(int number) {
+  if (number <= 1) return false;
+  for (int i = 2; i < number; i++) {
+    if (number % i == 0) return false;
+  }
+  return true;
 }
